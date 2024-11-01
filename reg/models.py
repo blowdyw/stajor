1a0761b3328408792044031f23d430ccc6e4861f
blowdyw <noobakkot@gmail.com>
2024-11-01 16:39:51 +0600

    first commitwerty

---
+++ reg/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """Создание нового пользователя с заданным email и паролем."""
+        if not email:
+            raise ValueError("Email должен быть указан.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Создание суперпользователя с заданным email и паролем."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Суперпользователь должен иметь is_staff=True.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Суперпользователь должен иметь is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, blank=False)  # Поле email, обязательное и уникальное
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)  # Поле для суперпользователя
+
+    USERNAME_FIELD = 'email'  # Используем email как имя пользователя
+    REQUIRED_FIELDS = []  # Оставьте пустым, если не требуется дополнительных полей
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
